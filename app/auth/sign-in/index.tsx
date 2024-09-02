4a5e24783243eb40516a54c1b0f251c0e669271c
Abishkar dhenga <Aabiskardhenga29@gmai.com>
2024-09-02 22:11:07 +0545

    firebase setup done and singin and sign up done

--- app/auth/sign-in/index.tsx
+++ app/auth/sign-in/index.tsx
@@ -6,18 +6,41 @@ import {
   TouchableOpacity,
   View,
 } from "react-native"
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import { useNavigation, useRouter } from "expo-router"
 import { Colors } from "@/constants/Colors"
+import { signInWithEmailAndPassword } from "firebase/auth"
+import { auth } from "@/configs/firebase.config"
 
 const Signin = () => {
   const router = useRouter()
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
   const navigation = useNavigation()
   useEffect(() => {
     navigation.setOptions({
       headerShown: false,
     })
   }, [])
+
+  const onSignin = () => {
+    if (!email && !password) {
+      alert("enter all the field")
+      return
+    }
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user
+        console.log("sign in user ", user)
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code
+        const errorMessage = error.message
+        console.log("error ", errorCode)
+      })
+  }
   return (
     <SafeAreaView>
       <View
@@ -31,7 +54,7 @@ const Signin = () => {
             fontFamily: "outfit",
             fontSize: 30,
             color: Colors.GRAY,
-            marginTop: 20,
+            marginTop: 10,
           }}
         >
           Welcome Back
@@ -41,24 +64,32 @@ const Signin = () => {
             fontFamily: "outfit",
             color: Colors.GRAY,
             fontSize: 30,
-            marginTop: 20,
+            marginTop: 10,
           }}
         >
           You've been missed .
         </Text>
         <View style={{ marginTop: 30 }}>
           <Text style={{ fontFamily: "outfit" }}> Email </Text>
-          <TextInput style={styles.input} placeholder="Enter the email" />
+          <TextInput
+            style={styles.input}
+            onChangeText={(e) => setEmail(e)}
+            placeholder="Enter the email"
+            value={email}
+          />
         </View>
         <View style={{ marginTop: 30 }}>
           <Text style={{ fontFamily: "outfit" }}> Password </Text>
           <TextInput
             secureTextEntry={true}
+            value={password}
             style={styles.input}
+            onChangeText={(e) => setPassword(e)}
             placeholder="Enter the Password"
           />
         </View>
         <TouchableOpacity
+          onPress={() => onSignin()}
           style={{
             backgroundColor: Colors.PRIMARY,
             borderRadius: 12,
--- app/auth/sign-up/index.tsx
+++ app/auth/sign-up/index.tsx
@@ -1,14 +1,153 @@
-import { StyleSheet, Text, View } from "react-native"
-import React from "react"
+import {
+  Alert,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native"
+import React, { useEffect, useState } from "react"
+import { useNavigation, useRouter } from "expo-router"
+import { Colors } from "@/constants/Colors"
+import Ionicons from "@expo/vector-icons/Ionicons"
+import { createUserWithEmailAndPassword } from "firebase/auth"
+import { auth } from "@/configs/firebase.config"
+import Toast from "react-native-toast-message"
+import { useToast } from "react-native-toast-notifications"
 
 const Signup = () => {
+  const router = useRouter()
+  const toast = useToast()
+
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [fullName, setFullName] = useState("")
+  const navigation = useNavigation()
+  useEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    })
+  }, [navigation])
+
+  const onCreateAccount = () => {
+    if (!email && !fullName && !password) {
+      alert("enter all the field")
+      return
+    }
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed up
+        const user = userCredential.user
+        console.log("user ", user)
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code
+        const errorMessage = error.message
+        console.log("error  ", errorMessage)
+
+        // ..
+      })
+  }
   return (
-    <View>
-      <Text>index</Text>
-    </View>
+    <SafeAreaView>
+      <View
+        style={{ backgroundColor: Colors.WHITE, height: "100%", padding: 15 }}
+      >
+        <Text
+          style={{
+            fontFamily: "outfit-bold",
+            color: Colors.PRIMARY,
+            fontSize: 30,
+            marginTop: 5,
+          }}
+        >
+          Create New Account
+        </Text>
+        <View style={{ marginTop: 25 }}>
+          <Text style={{ fontFamily: "outfit" }}> Full Name </Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={(e) => setFullName(e)}
+            value={fullName}
+            placeholder="Enter the Full  Name"
+          />
+        </View>
+        <View style={{ marginTop: 15 }}>
+          <Text style={{ fontFamily: "outfit" }}> Email </Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={(e) => setEmail(e)}
+            placeholder="Enter the email"
+            value={email}
+          />
+        </View>
+        <View style={{ marginTop: 15 }}>
+          <Text style={{ fontFamily: "outfit" }}> Password </Text>
+          <TextInput
+            secureTextEntry={true}
+            value={password}
+            style={styles.input}
+            onChangeText={(e) => setPassword(e)}
+            placeholder="Enter the Password"
+          />
+        </View>
+        <TouchableOpacity
+          onPress={() => onCreateAccount()}
+          style={{
+            backgroundColor: Colors.PRIMARY,
+            borderRadius: 12,
+            padding: 15,
+            marginTop: 30,
+          }}
+        >
+          <Text
+            style={{
+              color: Colors.WHITE,
+              textAlign: "center",
+              fontFamily: "outfit",
+              fontSize: 18,
+            }}
+          >
+            Create a Account
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => router.replace("/auth/sign-in")}
+          style={{
+            backgroundColor: Colors.WHITE,
+            borderRadius: 12,
+            padding: 15,
+            marginTop: 15,
+            borderWidth: 1,
+          }}
+        >
+          <Text
+            style={{
+              color: Colors.PRIMARY,
+              textAlign: "center",
+              fontFamily: "outfit",
+              fontSize: 18,
+            }}
+          >
+            Sign in
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   )
 }
 
 export default Signup
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  input: {
+    padding: 15,
+    borderWidth: 1,
+    borderRadius: 12,
+    borderColor: Colors.GRAY,
+    fontFamily: "outfit",
+    marginTop: 10,
+  },
+})
